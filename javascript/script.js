00ff4e6cc1efd018f2f4316eaad31a9bf1cffd6c
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-09 17:01:27 -0300

    atualizando com variáveis

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,12 @@
 
+// alert("Olá, Mundo! Bem-vindo ao NodeJS"); - Só funciona em navegadores, não em NodeJS
+
+idade = 62;
+
 console.log("Hello, World! Bem-vindo ao NodeJS");
 
-// alert("Olá, Mundo! Bem-vindo ao NodeJS"); - Só funciona em navegadores, não em NodeJS
+console.log("A idade é: ", idade);
+
+idade = 63;
+
+console.log("A idade depois do aniversário é: ", idade);
