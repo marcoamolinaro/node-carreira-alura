9018761954f2ecd3f68add623251d1a7ecd4d529
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-09 17:39:38 -0300

    Tipos do JS para variáveis

---
+++ javascript/tipos.js
@@ -0,0 +1,20 @@
+let nome = "João"
+let idade = 30
+let altura = 1.75
+
+console.log("typeof nome: ", typeof nome) // string
+console.log("typeof idade: ", typeof idade) // number
+console.log("typeof altura: ", typeof altura) // number 
+
+let estaChovendo = true // false or true
+console.log("typeof estaChovendo: ", typeof estaChovendo) // boolean
+
+let cidade
+
+console.log("typeof cidade: ", typeof cidade) // undefined
+
+let jaAprendeuJavaScript = null
+console.log("typeof jaAprendeuJavaScript: ", typeof jaAprendeuJavaScript) // object (null é considerado um objeto em JavaScript)
+
+let simbolo = Symbol()
+console.log("typeof simbolo: ", typeof simbolo) // symbol
