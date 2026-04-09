0ae97361eb116e4e9fc0e08298d146981c463a56
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-09 16:20:15 -0300

    Primeiro script no VS Code

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,4 @@
 
-console.log("Hello, World! This is a simple JavaScript file.");
+console.log("Hello, World! Bem-vindo ao NodeJS");
+
+// alert("Olá, Mundo! Bem-vindo ao NodeJS"); - Só funciona em navegadores, não em NodeJS
