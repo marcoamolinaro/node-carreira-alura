b1cb896e2432004f5c0aaefad91e157693e49d28
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-09 17:13:32 -0300

    formas diferentes de declarar variaveis

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,21 @@
 
 // alert("Olá, Mundo! Bem-vindo ao NodeJS"); - Só funciona em navegadores, não em NodeJS
 
-idade = 62;
+// Formas diferetes de declarar variáveis em JavaScript
+// 1 - Variaves Globais
+// 2 - var  
+// 3 - let
+// 4 - const
+
+let idade = 62;
 
 console.log("Hello, World! Bem-vindo ao NodeJS");
 
 console.log("A idade é: ", idade);
 
+const dataAniversario = "01/01/1900";
+
 idade = 63;
 
 console.log("A idade depois do aniversário é: ", idade);
+console.log("A data de aniversário é: ", dataAniversario);
