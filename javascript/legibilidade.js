f24f175f61714b649a35c49495e02d49c63271cd
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-09 17:57:47 -0300

    Legibilidade dos nomes das variáveis

---
+++ javascript/legibilidade.js
@@ -0,0 +1,7 @@
+// letras maiusculas e minusculas fazem diferença - case sensitive
+let preco = 19.90
+let Preco = 29.90
+
+console.log("O preço é: ", preco) // 19.90
+console.log("O Preço é: ", Preco) // 29.90
+
