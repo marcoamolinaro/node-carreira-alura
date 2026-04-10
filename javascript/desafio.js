915547829ff42c0226c38406528f5ad027ec7aaf
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-10 17:33:37 -0300

    Desafio troco

---
+++ javascript/desafio.js
@@ -0,0 +1,11 @@
+console.log('Boas vindas a calculadora de troco!')
+
+let valorCompra = 17.50
+let valorPago = 20.00
+
+console.log("Valor da compra R$: ", valorCompra) // 17.50
+console.log("Valor pago R$: ", valorPago) // 20.00
+
+let troco = valorPago - valorCompra
+
+console.log("O troco é R$: ", troco) // 2.50
