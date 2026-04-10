c33e2e3f2348546b6cffd21cae90de86fab41a75
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-10 17:16:15 -0300

    Comparação

---
+++ javascript/operadoresComparacao.js
@@ -0,0 +1,20 @@
+const idade = 36
+const minhaIdade = 62
+
+const comparacao = idade == minhaIdade
+
+console.log("São iguais: ", comparacao) // false
+
+const comparacao1 = idade < minhaIdade
+
+console.log("É menor: ", comparacao1) // true
+
+const comparacao2 = idade > minhaIdade
+
+console.log("É maior: ", comparacao2) // false
+
+const Idade1 = 36
+
+const comparacao3 = idade <= Idade1
+
+console.log("É menor ou igual: ", comparacao3) // true
