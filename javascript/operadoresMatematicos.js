b3702d59d2e32e711417b26dcadf0cac1b755469
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-10 16:59:11 -0300

    Operações aritiméticas

---
+++ javascript/operadoresMatematicos.js
@@ -0,0 +1,66 @@
+const numeroUm = 4
+const numeroDois = 8
+
+const subtracao = numeroUm - numeroDois
+
+console.log("Subtração: ", subtracao) // 4
+
+const soma = numeroUm + numeroDois
+
+console.log("Soma: ", soma) // 12   
+
+const divisao = numeroUm / numeroDois
+
+console.log("Divisão: ", divisao) // 0.5
+
+const multiplicacao = numeroUm * numeroDois
+
+console.log("Multiplicação: ", multiplicacao) // 32
+
+const contaComplexa = numeroUm + numeroDois * numeroUm
+
+console.log("Conta Complexa: ", contaComplexa) // 36
+
+const contaComplexa1 = (numeroUm + numeroDois) * numeroUm
+
+console.log("Conta Complexa1: ", contaComplexa1) // 48
+
+const resto = 13 % 2
+
+console.log("Resto: ", resto) // 1
+
+const divisao1 = 13 / 2
+
+console.log("Divisão : ", divisao1) // 6.5
+
+const potencia = 2 ** 3
+
+console.log("Potência: ", potencia) // 8
+
+let contador = 5
+
+contador = contador + 1
+
+console.log("Incremento: ", contador) // 6  
+
+contador++ // operador de incremento
+
+console.log("Incremento: ", contador) // 7
+
+contador = contador - 1
+
+console.log("Decremento: ", contador) // 6
+
+contador-- // operador de decremento
+
+console.log("Decremento: ", contador) // 5  
+
+let numeroTres = 2
+
+numeroTres = numeroTres * 2
+
+console.log("Atribuição de multiplicação: ", numeroTres) // 6  
+
+numeroTres *= 2 // operador de atribuição de multiplicação
+
+console.log("Atribuição de multiplicação: ", numeroTres) // 12
