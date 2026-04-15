8ad80bc352155c99cfdae10a22ddd6bcdd556d33
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2026-04-15 17:03:36 -0300

    Laço while...do while

---
+++ javascript/lacoEnquanto.js
@@ -0,0 +1,13 @@
+let contador = 1
+
+while (contador <= 10) {
+    console.log('Número Atual: ' ,  contador)
+    contador++
+}
+
+let numero = 1
+
+do {
+    console.log('Número Atual: ' ,  numero)
+    numero++
+} while (numero <= 10)
